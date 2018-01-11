04105e116f04ecdf8870559dbb2f5bfee29b6627
tunaico <31306056+tunaico@users.noreply.github.com>
2018-01-11 09:39:32 +0700

    Add files via upload

---
+++ ad_google_post.js
@@ -0,0 +1,10 @@
+&lt;script async src=&quot;//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js&quot;&gt;&lt;/script&gt;
+&lt;!-- responsive android --&gt;
+&lt;ins class=&quot;adsbygoogle&quot;
+     style=&quot;display:block&quot;
+     data-ad-client=&quot;ca-pub-8460239499593338&quot;
+     data-ad-slot=&quot;4231642673&quot;
+     data-ad-format=&quot;auto&quot;&gt;&lt;/ins&gt;
+&lt;script&gt;
+(adsbygoogle = window.adsbygoogle || []).push({});
+&lt;/script&gt;
