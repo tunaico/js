f62c82df04afbbf8d05996d40b633d27116c2889
tunaico <31306056+tunaico@users.noreply.github.com>
2018-01-11 09:20:34 +0700

    Add files via upload

---
+++ ad_google_intext.js
@@ -0,0 +1,10 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- responsive android -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-8460239499593338"
+     data-ad-slot="4231642673"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
